c3f517d915f13093e55e19532cd20218af09388d
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-08-13 22:56:15 +0530

    Update about page contents

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,7 +4,20 @@ const About = () => {
     return (
         <div className="about-container">
             <h1>About COSC</h1>
-            <p>Learn more about our team and mission on this page.</p>
+            <p>
+                COSC, an esteemed tech community based in Chaitanya Bharathi
+                Institute of Technology - Hyderabad, is dedicated to promoting
+                an open-source ethos. We organize a diverse range of events such
+                as hackathons, bootcamps, and workshops, aimed at educating
+                students about various technologies while actively fostering a
+                culture of open source. In these dynamic gatherings, students
+                immerse themselves in collaborative coding sessions, innovative
+                problem-solving challenges, and hands-on experiences, all
+                centered around open source principles. By embracing this
+                approach, we empower students to not only learn about
+                cutting-edge technologies but also actively contribute to their
+                development and dissemination..
+            </p>
         </div>
     );
 };
